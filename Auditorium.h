6df0f7ad7d0185cae7826fa64ccc07ad73c666d1
hk10877 <118066880+hk10877@users.noreply.github.com>
2024-10-01 14:07:46 -0500

    Update Auditorium.h

--- Auditorium.h
+++ Auditorium.h
@@ -1,11 +1,11 @@
 #ifndef AUDITORIUM_H
- #define AUDITORIUM_H
- #include <string>
+#define AUDITORIUM_H
+#include <string>
 #include "Node.h"
 
 class Auditorium 
 {
-  public:
+  private:
   Node* head;
   std::string totalAudiLine;
   char seatType;
@@ -17,13 +17,14 @@ public: //public functions
     Auditorium(std::string totalAudiLine); //constructors
     void insertNodeNext(char data, int row, int col);
     void insertNodeDown(char dataAudi, int row, int col);
-    void printAuditorium();
+    void printAuditorium(std::string fileName);
     void displayPrintAuditorium();
     int getRows();
     int getCols();
     bool isAvailable(int row, char seat, int totalQ);
     void reserveSeatsInAuditorium(int row, char seat, int adultQ, int childQ, int seniorQ);
-    void displayReport(); 
+    char unReserveSeatsInAuditorium(int row, char seat);
+    std::string displayAdminReport(); 
     std::string bestAvailable(int totalQuantity);
     friend std::ostream& operator<<(std::ostream& ostr, const Auditorium& obj);
     
